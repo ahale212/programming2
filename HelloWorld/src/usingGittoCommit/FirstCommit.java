f6f6159b7043ab4a29f378d7e86e4a41c2adc889
ahale212 <dalefarm@hotmail.co.uk>
2015-02-23 23:42:40 +0000

    First commit attempt using eclipse....

---
+++ HelloWorld/src/usingGittoCommit/FirstCommit.java
@@ -0,0 +1,10 @@
+package usingGittoCommit;
+
+public class FirstCommit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("used Git to commit this code straight from Eclipse");
+	}
+
+}
