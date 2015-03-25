d34f32e0b210efe06334ca6619be80348acad9ec
ahale212 <dalefarm@hotmail.co.uk>
2015-03-25 11:59:24 +0000

    sql create table script and then populating script

---
+++ create_table.sql
@@ -0,0 +1,12 @@
+drop table if exists patients;
+
+
+CREATE table patients(
+NHS_number numeric (20),
+title varchar (10),
+first_name char (20),
+last_name char (20),
+address varchar (60),
+contact_telephone_number varchar (15),
+known_allergies varchar (100),
+blood_group varchar (10));
---
+++ patients_details_script.sql
@@ -0,0 +1,37 @@
+INSERT INTO patients
+VALUES (1010101010,"Mr","Tom","Jones","123 Hollywood Avenue",'02844376211',"null","O+"),
+(1010101011,"Mrs", "Cheryl", "Cole", "234 Hollywood Avenue", '02811139467', "penicillin", "AB-"),
+(1010101012,"Master", "Jaden", "Smith", "93 Miami Walk", '07938817263', "milk", "AB+"),
+(1010101013,"General", "Abraham", "Lincoln", "100a White House", '00809128364',"nuts", "B-"),
+(1010101014, "Miss", "Amy", "Jones", "76 Newplace Sydney", '09913492783', "penicillin", "O-"),
+(1010101015, "Mr", "Alan", "Patterson", "123 Coconut Road", '83650091833', "null", "A-"),
+(1010101016, "Mrs", "Laura", "Steele", "3 Chestnut Drive", '02897541259', "null", "B-"),
+(1010101017, "Miss", "Clare", "Child", "17 Boardwalk Newcastle", '07988475842', "nuts, penicillin", "B+"),
+(1010101018, "Rev", "Ted", "Bishop", "10 Kennedy Walk", '12345678910', "dairy", "A+"),
+(1010101019, "Mr", "Winston", "Wolfe", "86c Toy Ota", '02899881234', "null", "A-"),
+(1010101020, "Mr", "Charles", "Darwin", "12 Doodoo Heights", '07512396773', "cats", "B-"),
+(1010101021, "Mrs", "Hannah", "Lynn", "76 Church Street", '07765092736', "nuts", "B+"),
+(1010101022, "Master", "Kyle", "Reece", "88 Church Avenue", '02822458089', "penicillin", "B+"),
+(1010101023, "Mrs", "Holly", "Rich", "99 Farm Lane", '07812298374', "Hay Fever", "A+"),
+(1010101024, "Rev", "Maxine", "Power", "102 Church Road", '02844366928', "null", "O-"),
+(1010101025, "Mr", "Charlie", "Knocks", "99 Downpatrick Heights", '02844987394', "null", "A-"),
+(1010101026, "Miss", "Stacey", "McMoose", "2 Donegall Square", '07899203841', "nuts", "A+"),
+(1010101027, "Mr", "Gareth", "Chambers", "33 Bank Views", '02844963990', "cheese", "B+"),
+(1010101028, "Master", "Billy", "Bob", "14 Cannon Walk", '075009829102', "penicillin", "A-"),
+(1010101029, "Mrs", "Hannah", "Lynn", "3a Tower Street", '02822459012', "null", "B+"),
+(1010101030, "Colonel", "Jim", "Sandy", "95 Kentucky Street", '02844509812', "Hay Fever", "AB-"),
+(1010101031, "Miss", "Amy", "Millar", "102 Archer Street", '07655299017', "null", "A-"),
+(1010101032, "Mrs", "Chloe", "Brown", "22 Millview Road", '02991823318', "penicillin", "AB+"),
+(1010101033, "Mr", "Joe", "Blogs", "11 Crossroad Park", '07944283923', "null", "A+"),
+(1010101034, "Mr", "John", "McKee", "12 Nighthall Avenue", '02844912853', "nuts", "B-"),
+(1010101035, "Mrs", "Gillian", "McCall", "99 High View Road", '07293800194', "dogs", "A+"),
+(1010101036, "General", "Graeme", "Gill", "2 Navy Street", '02890268543', "sea food", "A-"),
+(1010101037, "Master", "James", "Raymond", "29 Blackpool Street", '07599082746', "null", "A+"),
+(1010101038, "Mr", "Ciaran", "Ward", "8a Riverhouse Appartments", '02990839126', "nuts", "B-"),
+(1010101039, "Master", "Adam", "Wallace", "The Newford Lane", '02844863994', "penicillin", "O+"),
+(1010101040, "Mrs", "Jay", "Simpson", "9 Bankview Place", '07762838144', "null", "A-"),
+(1010101041, "Admiral", "Simpson", "Ford", "46 Docking Street", '02890388243', "null", "A+"); 
+select * from patients;
+
+
+
