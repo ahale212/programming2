1c90762aa7250d34cafeaf36bb26840fc014500d
ahale212 <dalefarm@hotmail.co.uk>
2015-04-08 10:13:28 +0100

    adding jdbc. need to add external jars for it to work

---
+++ HelloWorld/src/application/JDBC.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.sql*;
+
+public class JDBC {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// establish connection to mySQl
+		String url = "jdbc:mysql://web2.eeecs.qub.ac.uk/40058483";
+		Connection con;
+		
+			}
+			// resultSet 1
+			ResultSet rs1 = updateSalary
+					.executeQuery(“SELECT * FROM Staff_Members WHERE title = ‘fx.getTitle’ ANDfirst_name = ‘fx.getFirstName’ AND  last_name = ‘fx.getLastName’”);
+			
+			
+			
+			
+			
+			//start of catch block
+		} catch (SQLException ex) {
+			System.err.println("SQLException: " + ex.getMessage());
+
+		}
+	}// end of main method
+	
+	
+
+
+
+	/**
+	 * @param
+	 * Method to display results from a search on fx
+	 * @return
+	 * The display of results
+	 */
+	
+	
+	}// end of displayResults method
+	
+}
