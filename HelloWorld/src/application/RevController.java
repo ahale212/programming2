d8f65ee1e98f9dcb3114a0fe378baa7df70bcba5
Ciarán <Ciarn@Kinky.home>
2015-04-09 15:43:17 +0100

    Commit: changes made to GUI; a. Menu bar (top) b. Button list (left side) c. Triage tab (bottom) d. Listviews (centre) for queue, oc unit, treatment rooms e. TextArea (right) for server feed

--- HelloWorld/src/application/RevController.java
+++ HelloWorld/src/application/RevController.java
@@ -14,16 +14,21 @@ import javafx.scene.control.TextField;
 public class RevController implements Initializable {
 	
 	@FXML
-	private ListView queue, trooms;
+	private ListView queue, trooms, treatment_room_list, on_call_list;
 	
 	@FXML
-	private Button RunLists;
+	private Button UPGRADE;
 	
 	private final ObservableList QList = FXCollections.observableArrayList();
 	private final ObservableList trList = FXCollections.observableArrayList();
+	private final ObservableList trno = FXCollections.observableArrayList();
+	private final ObservableList ocu = FXCollections.observableArrayList();
 	
 	private String[] array = new String[10];
 	private String[] array1 = new String[4];
+	private String[] onCall = new String[1];
+	private String[] treatmentRoomNum = new String[4];
+
 	
 	@Override
 	public void initialize(URL fxmlFilelocation, ResourceBundle resources) {
@@ -41,21 +46,31 @@ public class RevController implements Initializable {
 		array1[1] = "JonJoe";
 		array1[2] = "Joe";
 		array1[3] = "JJJoe";
+		onCall[0] = "On Call Unit";
+		treatmentRoomNum[0] = "Treatment Room 1";
+		treatmentRoomNum[1] = "Treatment Room 2";
+		treatmentRoomNum[2] = "Treatment Room 3";
+		treatmentRoomNum[3] = "Treatment Room 4";
 		
+		trno.addAll(treatmentRoomNum);
+		ocu.addAll(onCall);
 		QList.addAll(array);
 		trList.addAll(array1);
 		queue.setItems(QList);
-		
+		treatment_room_list.setItems(trno);
+		on_call_list.setItems(ocu);
+	
 		// DEMO Purposes only
 		// Select a name
 		// Click 'Tick' Button
 		// See the selected name replace another in the trList
-		RunLists.setOnAction(e -> {String potential = (String) queue.getSelectionModel().getSelectedItem();
+		UPGRADE.setOnAction(e -> {String potential = (String) queue.getSelectionModel().getSelectedItem();
 	      if (potential != null) {
 	        queue.getSelectionModel().clearSelection();
 	        QList.remove(potential);
 	        trList.remove(0);
-	        trList.add(trList.size()-1, potential);}});
+	        trList.add(3, "");
+	        trList.add(3, potential);}});
 		
 		trooms.setItems(trList);
 		
