8991fd32e4451a05e7f7876e92f9cecc44ebadbb
ahale212 <dalefarm@hotmail.co.uk>
2015-04-09 11:41:13 +0100

    added dob and postcode, tided everything in line to db

--- HQBertServer/src/uk/ac/qub/exjavaganza/hqbert/server/v01/Person.java
+++ HQBertServer/src/uk/ac/qub/exjavaganza/hqbert/server/v01/Person.java
@@ -8,14 +8,16 @@ import java.util.LinkedList;
 public class Person implements Serializable {
 	public enum Title { Mr, Mrs, Miss, Ms, Dr, Fr, Rev, Prof, Sir, Lady, General; }
 	public enum BloodGroup { APositive, ANegative , BPositive, BNegative, ABPositive, ABNegative, OPositive, ONegative };
-	String  nhsNum;
+	
+	String  NHSNum;
 	Title title;
 	String firstName;
 	String lastName;
+	String dateOfBirth;
 	String address1;
-	String address2;
 	String city;
 	String country;
+	String postcode;
 	String telephoneNum;
 	LinkedList<String> allergies;
 	BloodGroup bloodGroup;
@@ -27,10 +29,10 @@ public class Person implements Serializable {
 		this.firstName = firstName;
 	}
 	
-	public String getNhsNum() {
-		return nhsNum;
+	public String getNHSNum() {
+		return NHSNum;
 	}
-	public void setNhsNum(String nhsNum) {
-		this.nhsNum = nhsNum;
+	public void setNHSNum(String nhsNum) {
+		this.NHSNum = nhsNum;
 	}
 }
