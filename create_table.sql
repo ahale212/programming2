0e924d0b635ee2c6ee27896ba20f2193159944f0
ahale212 <dalefarm@hotmail.co.uk>
2015-04-13 10:29:46 +0100

    scripts

--- create_table.sql
+++ create_table.sql
@@ -9,4 +9,6 @@ last_name char (20),
 address varchar (60),
 contact_telephone_number varchar (15),
 known_allergies varchar (100),
-blood_group varchar (10));
+blood_group varchar (10),
+PRIMARY KEY (NHS_number)
+);
